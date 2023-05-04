e940d08cfaee4ca05392e4cafe69a47c931e45e7
VictorVanO <20253@ecam.be>
2023-05-04 17:35:07 +0200

    mqlsdkfjé

--- react-axios-typescript/src/services/jira_user.service.ts
+++ react-axios-typescript/src/services/jira_user.service.ts
@@ -1,6 +1,5 @@
 import http from "../http-common";
 import IJiraUserData from "../types/jira_user.type";
-import IJiraUserData from "../types/jira_user.type";
 
 class JiraUserDataService {
     getAll() {
