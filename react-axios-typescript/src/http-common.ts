647c829073dd331388e33629c479aac684b080f1
Mourad Mettioui <mouradm.mm@outlook.com>
2023-05-12 13:15:53 +0200

    clean

--- react-axios-typescript/src/http-common.ts
+++ react-axios-typescript/src/http-common.ts
@@ -2,7 +2,13 @@ import axios from "axios";
 
 export default axios.create({
 <<<<<<< HEAD
+<<<<<<< HEAD
   baseURL: "http://localhost:8085/api/v1",
+=======
+  // baseURL: "https://777b-81-164-82-169.ngrok-free.app",
+  baseURL: "https://777b-81-164-82-169.ngrok-free.app/api/v1/",
+  // baseURL: "http://localhost:8080/api/v1",
+>>>>>>> 82fd5ed094f91a9c8aa5057d6ab92442a2b7471c
 =======
   // baseURL: "https://777b-81-164-82-169.ngrok-free.app",
   baseURL: "https://777b-81-164-82-169.ngrok-free.app/api/v1/",
